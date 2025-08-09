1f00f5b6ea4bc17079484fe220692ed36b4e8ed7
Bhawuk Arora <bhawukarora042@gmail.com>
2025-08-09 22:54:46 +0530

    g2g

--- src/main/java/com/ecommerce/ordertracking/repository/UserRepository.java
+++ src/main/java/com/ecommerce/ordertracking/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.ecommerce.ordertracking.repository;
 
 import com.ecommerce.ordertracking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
